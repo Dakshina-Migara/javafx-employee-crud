92bc5ecbe605a9168c89a58aad8406db66e7e14b
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-06 22:52:45 +0530

    create the DeleteEmployeeController

---
+++ src/main/java/com/example/practice/controller/DeleteEmployeeController.java
@@ -0,0 +1,20 @@
+package com.example.practice.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class DeleteEmployeeController {
+    @FXML
+    private TextField txtNic;
+
+    @FXML
+    void back(ActionEvent event) {
+
+    }
+
+    @FXML
+    void delete(ActionEvent event) {
+
+    }
+}
