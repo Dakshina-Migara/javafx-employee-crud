55147bdcece0d54ad4e8577e9f7c4daa267d8b65
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-06 20:04:08 +0530

    first commit

---
+++ src/main/java/com/example/practice/controller/SaveEmployeeController.java
@@ -0,0 +1,25 @@
+package com.example.practice.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+
+public class SaveEmployeeController {
+    @FXML
+    private TextField txtAge;
+
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtNic;
+
+    @FXML
+    private TextField txtSalary;
+
+    @FXML
+    void save(ActionEvent event) {
+
+    }
+
+}
---
+++ src/main/java/com/example/practice/employeeDto/EmployeeDto.java
@@ -0,0 +1,50 @@
+package com.example.practice.employeeDto;
+
+public class EmployeeDto {
+    private String name;
+    private String nic;
+    private int age;
+    private boolean salary;
+
+    public EmployeeDto(String name, String nic, int age, boolean salary) {
+        this.name = name;
+        this.nic = nic;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public EmployeeDto() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getNic() {
+        return nic;
+    }
+
+    public void setNic(String nic) {
+        this.nic = nic;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public boolean isSalary() {
+        return salary;
+    }
+
+    public void setSalary(boolean salary) {
+        this.salary = salary;
+    }
+}
