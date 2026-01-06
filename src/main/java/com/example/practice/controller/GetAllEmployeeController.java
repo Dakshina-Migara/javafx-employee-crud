a08da469d1e1298ebb7e79091b801d72408c1bbf
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-07 01:28:40 +0530

    make the navigation for the page to page

--- src/main/java/com/example/practice/controller/GetAllEmployeeController.java
+++ src/main/java/com/example/practice/controller/GetAllEmployeeController.java
@@ -1,18 +1,26 @@
 package com.example.practice.controller;
 
+import com.example.practice.HelloApplication;
 import com.example.practice.employeeDto.EmployeeDto;
 import com.example.practice.service.EmployeeService;
 import com.example.practice.service.impl.EmployeeServiceImpl;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.List;
 
 public class GetAllEmployeeController {
 
+    @FXML
+    private AnchorPane root;
     @FXML
     private TableColumn<EmployeeDto, Integer> tblAge;
 
@@ -31,8 +39,14 @@ public class GetAllEmployeeController {
     EmployeeService employeeService = new EmployeeServiceImpl();
 
     @FXML
-    void back(ActionEvent event) {
+    void back(ActionEvent event) throws IOException {
+        Stage stage = (Stage) this.root.getScene().getWindow();
 
+        //yanna ona scene eka dagannawa
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("home-page.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+
+        stage.setScene(scene);
     }
 
     @FXML
