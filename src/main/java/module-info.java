71eebbdc87d5abcf44e65852b10b6d4e1afe1153
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-01-06 22:43:11 +0530

    export controller package to the module

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,9 @@
 module com.example.practice {
     requires javafx.controls;
     requires javafx.fxml;
-
+    requires java.sql;
 
     opens com.example.practice to javafx.fxml;
+    opens com.example.practice.controller to javafx.fxml;
     exports com.example.practice;
 }
