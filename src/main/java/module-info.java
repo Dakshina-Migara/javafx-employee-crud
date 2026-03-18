737a9bf30148e87859b460b8f034fe5753da58b1
Dakshina-Migara <dakshinamigara666@gmail.com>
2026-03-18 11:32:52 +0530

    add requires transitive for javafx.graphics in module-info
    Resolved IDE accessibility warning on the Stage class by changing 'requires javafx.graphics' to 'requires transitive javafx.graphics' so consuming modules have proper access.

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module com.example.practice {
     requires javafx.controls;
+    requires transitive javafx.graphics;
     requires javafx.fxml;
     requires java.sql;
     opens com.example.practice.controller to javafx.fxml;
